0f39f5e69d5aa5b3a51ac7f4815236e8d57e2015
SEYA-CODING <seyattv@gmail.com>
2025-11-10 00:08:23 +0000

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,29 @@
+-- ServerBeats v1.1.0 by SEYA CODING
+-- Discord: seya_coding #6497
+-- Community: https://discord.gg/RT3uJRdXSC
+
+Config = {}
+
+-- 🔊 Master toggle for the entire script
+Config.Enabled = true
+
+-- 👑 Steam/License identifiers of owners who can control music
+Config.Owners = {
+    -- Example: "steam:110000112345678",
+}
+
+-- 🔈 Default global volume (0.0–1.0)
+Config.DefaultVolume = 0.6
+
+-- 🎚️ Allow players to adjust their own local volume (true/false)
+Config.AllowPlayerVolume = true
+
+-- 🧾 Show credits in console
+Config.ShowCreditsInConsole = true
+
+-- 🧠 Framework detection (auto, qb, esx, or none)
+Config.ForceFramework = 'auto'
+
+-- 💻 NUI display size
+Config.NuiWidth = 600
+Config.NuiHeight = 180
